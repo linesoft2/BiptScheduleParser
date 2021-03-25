37edad74860a2b8270d25956224626224551372f
双霖 <l@linesoft.top>
2021-03-25 14:34:13 +0800

    Update biptParser1.js

--- biptParser1.js
+++ biptParser1.js
@@ -13,12 +13,12 @@ function scheduleHtmlParser(html) {
         let infoArr = value.eq(9).children().children().children()
         for (let n = 0; n < infoArr.length; n++) {
             let info = $(infoArr[n]).children()
-            let infoTextArr = info.text().trim().replace(/[\r\n]/g,"").split("                    ")
+            let infoTextArr = info.text().trim().replace(/[\r\n]/g, "").split("                    ")
             //["第1-5全周", "星期三", "第6,7节", "康404"]
             let weeks = createWeeks(infoTextArr[0])
             let week = createWeek(infoTextArr[1])
             let sections = createSections(infoTextArr[2])
-            let position = infoTextArr[3].replace("（）","")
+            let position = infoTextArr[3].replace("（）", "")
             result.push({
                 "name": courseName,
                 "position": position,
@@ -36,7 +36,7 @@ function scheduleHtmlParser(html) {
     return { courseInfos: result }
 }
 
-function createWeek(weekStr){
+function createWeek(weekStr) {
     switch (weekStr) {
         case "星期五":
             return 5;
@@ -65,61 +65,61 @@ function createWeek(weekStr){
     }
 }
 
-function createWeeks(weeksStr){
+function createWeeks(weeksStr) {
     //"第1-5全周"
-    if(weeksStr.indexOf("-")==-1){
-        return [parseInt(weeksStr.replace("单","").replace("双","").replace("全","").replace("第","").replace("周",""))]
+    if (weeksStr.indexOf("-") == -1) {
+        return [parseInt(weeksStr.replace("单", "").replace("双", "").replace("全", "").replace("第", "").replace("周", ""))]
     }
     let flag = 0 //0全周，1单周，2双周
-    if(weeksStr.indexOf("单周")!=-1){
+    if (weeksStr.indexOf("单周") != -1) {
         flag = 1
-    }else if(weeksStr.indexOf("双周")!=-1){
+    } else if (weeksStr.indexOf("双周") != -1) {
         flag = 2
     }
 
-    let end = parseInt(getMidValue(weeksStr,"-","周").replace("单","").replace("双","").replace("全",""))
+    let end = parseInt(getMidValue(weeksStr, "-", "周").replace("单", "").replace("双", "").replace("全", ""))
 
     let arr = []
-    for(let i = parseInt(getMidValue(weeksStr,"第","-"));i<=end;i++){
-        if(flag==0){
+    for (let i = parseInt(getMidValue(weeksStr, "第", "-")); i <= end; i++) {
+        if (flag == 0) {
             arr.push(i)
-        }else{
+        } else {
             let a = i % 2
-            if(a == 0){
-                if(flag==2){
+            if (a == 0) {
+                if (flag == 2) {
                     arr.push(i)
                 }
-            }else{
-                if(flag==1){
+            } else {
+                if (flag == 1) {
                     arr.push(i)
                 }
             }
         }
-        
+
     }
     return arr
 }
 
-function getMidValue(str,firstStr,secondStr){
-   if(str == "" || str == null || str == undefined){ // "",null,undefined
-       return "";
-   }
-   if(str.indexOf(firstStr)<0){
+function getMidValue(str, firstStr, secondStr) {
+    if (str == "" || str == null || str == undefined) { // "",null,undefined
         return "";
-   }
-   var subFirstStr=str.substring(str.indexOf(firstStr)+firstStr.length,str.length);
-   var subSecondStr=subFirstStr.substring(0,subFirstStr.indexOf(secondStr));
-   return subSecondStr;
+    }
+    if (str.indexOf(firstStr) < 0) {
+        return "";
+    }
+    var subFirstStr = str.substring(str.indexOf(firstStr) + firstStr.length, str.length);
+    var subSecondStr = subFirstStr.substring(0, subFirstStr.indexOf(secondStr));
+    return subSecondStr;
 }
 
- function createSections(str){
-     //第1,2节
-     let strArr = str.replace("第","").replace("节","").split(",")
-     let arr = []
-     for (let i =0; i< strArr.length;i++){
+function createSections(str) {
+    //第1,2节
+    let strArr = str.replace("第", "").replace("节", "").split(",")
+    let arr = []
+    for (let i = 0; i < strArr.length; i++) {
         arr.push({
             section: parseInt(strArr[i])
         })
-     }
-     return arr
- }
+    }
+    return arr
+}
